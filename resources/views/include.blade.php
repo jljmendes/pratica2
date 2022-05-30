02910c4f7957f797ea1067d3978d1667385d1752
jocabispo <jljmendesmanutencao@gmail.com>
2022-05-30 00:32:02 -0300

    Trabalhando com variações de Include

--- resources/views/include.blade.php
+++ resources/views/include.blade.php
@@ -1,2 +1,7 @@
 <h1>Iniciando pratica com include</h1>
-@include('components.component')
+{{-- Variações de Include --}}
+
+{{-- @includeWhen(false, 'components.component', ['html' => '<h1>Componente1</h1>'] ) --}}
+{{-- @includeWhen(false, 'components.component', ['html' => '<h1>Componente1</h1>'] ) --}}
+{{-- @includeIf('components.component1', ['html' => '<h1>Componente1</h1>'] ) --}}
+@includeUnlass(true,'components.component1', ['html' => '<h1>Componente1</h1>'] )
