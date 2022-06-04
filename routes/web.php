5f9c5edebbda5f51a4a97e9164496fd4694bda33
jocabispo <jljmendesmanutencao@gmail.com>
2022-06-03 23:25:58 -0300

    Trabalhando com layout2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [SiteController::class, 'index']);
 Route::get('/include', [SiteController::class, 'index2']);
 Route::get('/layout', [SiteController::class, 'layout']);
+Route::get('/layout2', [SiteController::class, 'layout2']);
 Route::get('/exercise', [SiteController::class, 'exercicio2']);
 Route::get('/sair', [SiteController::class, 'exit']);
 Route::get('/usuarios/{qtd}', [SiteController::class, 'users']);
